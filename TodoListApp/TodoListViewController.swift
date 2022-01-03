3765657074910a2e72d0d2a0af27e01395844f27
plateauh <70834431+plateauh@users.noreply.github.com>
2022-01-03 11:28:30 +0300

    Persistent data yaaay

--- TodoListApp/TodoListViewController.swift
+++ TodoListApp/TodoListViewController.swift
@@ -6,33 +6,55 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController, SingleItemViewControllerDelegate {
     
-    var items = ["Najd", "Alseghair"]
-    var details = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. ", "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."]
-    var dates = ["01/03/2022", "01/04/2022"]
+    let managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let saveContext = (UIApplication.shared.delegate as! AppDelegate).saveContext
+    var items = [Item]()
     
     func addItem(title: String, detials: String, date: String, isDone: Bool) {
-        items.append(title)
-        details.append(detials)
-        dates.append(date)
+        let item = NSEntityDescription.insertNewObject(forEntityName: "Item", into: managedObjectContext) as! Item
+        item.title = title
+        item.details = detials
+        item.due = date
+        item.isDone = isDone
+        items.append(item)
+        saveContext()
         dismiss(animated: true, completion: nil)
         tableView.reloadData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        retrieveAllItems()
+    }
+    
+    func retrieveAllItems() {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+            items = try managedObjectContext.fetch(request)
+        } catch {
+            print(error)
+        }
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         items.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as! ItemViewCell
-        cell.itemTitle.text = items[indexPath.row]
-        cell.itemDetails.text = details[indexPath.row]
-        cell.itemDate.text = dates[indexPath.row]
+        cell.itemTitle.text = items[indexPath.row].title
+        cell.itemDetails.text = items[indexPath.row].details
+        cell.itemDate.text = items[indexPath.row].due
+        if items[indexPath.row].isDone {
+            cell.accessoryType = .checkmark
+        }
+        else {
+            cell.accessoryType = .none
+        }
         return cell
     }
     
@@ -40,17 +62,22 @@ class TodoListViewController: UITableViewController, SingleItemViewControllerDel
         if let cell = tableView.cellForRow(at: indexPath) {
             if cell.accessoryType == .checkmark {
                 cell.accessoryType = .none
+                items[indexPath.row].isDone = false
             }
             else {
                 cell.accessoryType = .checkmark
+                items[indexPath.row].isDone = true
             }
+            saveContext()
+            tableView.reloadData()
             }
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let item = items[indexPath.row]
+        managedObjectContext.delete(item)
+        saveContext()
         items.remove(at: indexPath.row)
-        details.remove(at: indexPath.row)
-        dates.remove(at: indexPath.row)
         tableView.reloadData()
     }
     
